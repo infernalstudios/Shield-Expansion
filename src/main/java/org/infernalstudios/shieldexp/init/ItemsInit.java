4734f985548aff9808085a8a21e1e809beec2f49
nekomaster1000 <nekozzz@live.co.uk>
2022-10-13 02:19:48 +0100

    Added functional models and crafting recipes for new shields
    - The new Shields now all display correctly in-game! They look great!
    - Switched the movement speed penalties of the Wooden and Golden shields, made gold shield deal no damage back on a parry

--- src/main/java/org/infernalstudios/shieldexp/init/ItemsInit.java
+++ src/main/java/org/infernalstudios/shieldexp/init/ItemsInit.java
@@ -17,12 +17,12 @@ public class ItemsInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ShieldExpansion.MOD_ID);
     public static final List<RegistryObject<NewShieldItem>> SHIELDS = new ArrayList<>();
 
-    //movement speed factors are modified to align with the vanilla shield modifier, actual results are 90%, 75%, 60%, 45%, and 25% as intended
-    public static final RegistryObject<NewShieldItem> WOODEN_SHIELD = registerShield("wooden_shield", 55, 0.9F, 0.0F);
-    public static final RegistryObject<NewShieldItem> GOLDEN_SHIELD = registerShield("golden_shield", 77, 0.78F, 0.15F);
-    public static final RegistryObject<NewShieldItem> IRON_SHIELD = registerShield("iron_shield", 165, 0.64F, 0.30F);
-    public static final RegistryObject<NewShieldItem> DIAMOND_SHIELD = registerShield("diamond_shield", 363, 0.55F, 0.50F);
-    public static final RegistryObject<NewShieldItem> NETHERITE_SHIELD = registerShield("netherite_shield", 607, 0.38F, 0.75F);
+    //movement speed factors are modified to align with the vanilla shield modifier, actual results are 75%, 90%, 60%, 45%, and 25% as intended
+    public static final RegistryObject<NewShieldItem> WOODEN_SHIELD = registerShield("wooden_shield", 55, 0.78F, 0.0F);
+    public static final RegistryObject<NewShieldItem> GOLDEN_SHIELD = registerShield("golden_shield", 77, 0.9F, 0.00F);
+    public static final RegistryObject<NewShieldItem> IRON_SHIELD = registerShield("iron_shield", 165, 0.64F, 0.20F);
+    public static final RegistryObject<NewShieldItem> DIAMOND_SHIELD = registerShield("diamond_shield", 363, 0.55F, 0.35F);
+    public static final RegistryObject<NewShieldItem> NETHERITE_SHIELD = registerShield("netherite_shield", 607, 0.38F, 0.50F);
 
     public static RegistryObject<NewShieldItem> registerShield(String id, int durability, float speedFactor, float damageBack) {
         RegistryObject<NewShieldItem> shield = ITEMS.register(id, () -> new NewShieldItem(new Item.Properties().durability(durability).tab(CreativeModeTab.TAB_COMBAT), speedFactor, damageBack));
