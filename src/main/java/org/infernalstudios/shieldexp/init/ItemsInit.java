ad35ddaf1723a113024b7e6dd349673c98c47107
nekomaster1000 <nekozzz@live.co.uk>
2022-10-15 03:22:18 +0100

    Updated Shield blocking models, movement speeds and texture
    - Added the render banner to the mods.toml file
    - Last commit before release!?

--- src/main/java/org/infernalstudios/shieldexp/init/ItemsInit.java
+++ src/main/java/org/infernalstudios/shieldexp/init/ItemsInit.java
@@ -17,12 +17,12 @@ public class ItemsInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ShieldExpansion.MOD_ID);
     public static final List<RegistryObject<NewShieldItem>> SHIELDS = new ArrayList<>();
 
-    //movement speed factors are modified to align with the vanilla shield modifier, actual results are 75%, 90%, 60%, 45%, and 25% as intended
+    //movement speed factors are modified to align with the vanilla shield modifier, actual results are 75%, 90%, 60%, 40~%, and 20~% as intended
     public static final RegistryObject<NewShieldItem> WOODEN_SHIELD = registerShield("wooden_shield", 55, 20, 0.78F, 0.0F, 5);
     public static final RegistryObject<NewShieldItem> GOLDEN_SHIELD = registerShield("golden_shield", 32, 25, 0.9F, 0.0F, 6);
-    public static final RegistryObject<NewShieldItem> IRON_SHIELD = registerShield("iron_shield", 165, 30, 0.64F, 0.20F, 5);
-    public static final RegistryObject<NewShieldItem> DIAMOND_SHIELD = registerShield("diamond_shield", 363, 45, 0.55F, 0.35F, 4);
-    public static final RegistryObject<NewShieldItem> NETHERITE_SHIELD = registerShield("netherite_shield", 607, 60, 0.38F, 0.50F, 3);
+    public static final RegistryObject<NewShieldItem> IRON_SHIELD = registerShield("iron_shield", 165, 30, 0.60F, 0.20F, 5);
+    public static final RegistryObject<NewShieldItem> DIAMOND_SHIELD = registerShield("diamond_shield", 363, 45, 0.38F, 0.35F, 4);
+    public static final RegistryObject<NewShieldItem> NETHERITE_SHIELD = registerShield("netherite_shield", 607, 60, 0.25F, 0.50F, 3);
 
     public static RegistryObject<NewShieldItem> registerShield(String id, int durability, int blockTicks, float speedFactor, double damageBack, int parryTicks) {
         if (id.equals("netherite_shield")) {
